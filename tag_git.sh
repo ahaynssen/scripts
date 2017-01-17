1f25d7753041decdf79c8e9deb6bb347d0aff9e9
ahaynssen <andy.haynssen@objectpartners.com>
2017-01-17 12:03:02 -0600

    populate for tests

--- tag_git.sh
+++ tag_git.sh
@@ -3,4 +3,4 @@
 # Infer version, then increment
 VERSION=0.1.0
 API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
-curl --data "$API_JSON" https://api.github.com/repos/ahaynssen/scripts/releases?access_token=3f1d08976c5591d260790468eb6ce9adf5d7598a
+curl --data "$API_JSON" https://api.github.com/repos/ahaynssen/scripts/releases?access_token=$TOKEN
